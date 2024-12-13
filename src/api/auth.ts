50e9dd9aaab05a571244fbead72b3eee4546186c
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-13 22:26:48 +0700

    Returen user data after login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,8 +3,18 @@ import supabase from '../utils/supabase';
 export const login = async (email: string, password: string) => {
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) throw error;
-  console.log("User logged in:", { data });
-  return data;
+
+  const userId = data.user?.id;
+  if (!userId) throw new Error('User ID not found');
+
+  const { data: userData, error: userError } = await supabase
+    .from('users')
+    .select('*')
+    .eq('user_id', userId)
+    .single();
+  if (userError) throw userError;
+
+  return userData;
 };
 
 export const logout = async () => {
