dad484b3f726b993f794536fe363bb3d0a17d2da
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-13 22:30:36 +0700

    Updated login screen UIUX, implemented Redux, included location permission component, and added logo

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,13 +1,11 @@
+import { setUser } from "../store";
 import { login } from "../api/auth";
-import * as Notifications from 'expo-notifications';
+import { useDispatch } from "react-redux";
+import * as Location from 'expo-location';
 import React, { useState, useEffect } from "react";
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { IconRegistry } from '@ui-kitten/components';
-import { useFocusEffect } from '@react-navigation/native';
-import { Input, Button, Text } from '@ui-kitten/components';
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View, Alert, StyleSheet, Platform, Linking } from "react-native";
 import LocationPermissionModal from "../components/LocationPermission";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { View, Text, TextInput, Button, Alert, Platform, Linking, Image, TouchableOpacity } from "react-native";
 
 type RootStackParamList = {
   Login: undefined;
@@ -17,21 +15,11 @@ type RootStackParamList = {
 type Props = NativeStackScreenProps<RootStackParamList, "Login">;
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [permissionsGranted, setPermissionsGranted] = useState(false);
-  const [permissionError, setPermissionError] = useState<string | null>(null);
 
-  const handleLogin = async () => {
-    try {
-      await login(email, password);
-      navigation.navigate("Main");
-    } catch (error: any) {
-      Alert.alert("Login failed", error.message);
-    }
-  };
-
-  // Function to check if location permissions are granted
   const openAppSettings = () => {
     if (Platform.OS === 'ios') {
       Linking.openURL('app-settings:');
@@ -40,95 +28,104 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
     }
   };
 
-  console.log("LoginScreen rendered");
+  const handleLogin = async () => {
+    try {
+      const user = await login(email, password);
+      dispatch(setUser(user));
+      navigation.navigate("Main");
+    } catch (error: any) {
+      Alert.alert("Login failed", error.message);
+    }
+  };
+
+  const handlePermissionsGranted = () => {
+    setPermissionsGranted(true);
+  };
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      const { status: foreground } = await Location.getForegroundPermissionsAsync();
+      const { status: background } = await Location.getBackgroundPermissionsAsync();
+
+      if (foreground === 'granted' && background === 'granted') {
+        setPermissionsGranted(true);
+      } else {
+        setPermissionsGranted(false);
+      }
+    };
+
+    checkPermissions();
+  }, []);
 
   return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <View style={styles.container}>
-        <Text style={styles.title}>Login</Text>
+    <View className="flex-1 bg-[#f5f5f5] mt-4 px-6 py-6 justify-between">
+      {/* Logo Perusahaan (Posisi di atas, tengah) */}
+      <View className="items-center">
+        <Image
+          source={require("../../assets/mdm-logo.png")}
+          style={{ width: 50, height: 50 }}
+          resizeMode="contain"
+        />
+      </View>
 
-        {/* Error Text */}
-        {permissionError && (
-          <Text style={styles.errorText}>{permissionError}</Text>
-        )}
+      {/* Logo Produk (Di antara logo perusahaan dan form login) */}
+      <View className="items-center justify-center">
+        <Image
+          source={require("../../assets/pasti-tracking-logo-new.png")}
+          style={{ width: "85%", height: 120 }}
+          resizeMode="contain"
+        />
+      </View>
 
-        {/* Button to open Settings */}
-        {
-          !permissionsGranted && (
-            <Button
-              status="basic"
-              onPress={openAppSettings}
-              style={styles.button}
-            >
-              Open Settings
-            </Button>
-          )
-        }
-
+      {/* Form Login (Tengah layar secara vertikal) */}
+      <View className="w-full max-w-md bg-white shadow-lg rounded-lg p-8 justify-start">
+        <Text className="text-center mb-5 text-2xl font-bold text-[#84439b]">Login</Text>
         {/* Input fields */}
-        <Input
-          label="Email"
+        <TextInput
+          placeholder="Email"
           value={email}
           onChangeText={setEmail}
           keyboardType="email-address"
           autoCapitalize="none"
-          style={styles.input}
-          disabled={!permissionsGranted}
+          className="bg-white border border-gray-300 rounded-md p-3 mb-4"
+          editable={permissionsGranted}
         />
-        <Input
-          label="Password"
+        <TextInput
+          placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
-          style={styles.input}
-          disabled={!permissionsGranted}
+          className="bg-white border border-gray-300 rounded-md p-3 mb-6"
+          editable={permissionsGranted}
         />
-
         {/* Login Button */}
         <Button
+          title="Login"
           onPress={handleLogin}
-          style={styles.button}
+          color="#84439b"
           disabled={!permissionsGranted}
-        >
-          Login
-        </Button>
+        />
       </View>
 
+      {/* Error Text */}
+      {!permissionsGranted && (
+        <Text className="text-red-500 text-center mb-4">Permission Required</Text>
+      )}
+
+      {/* Button to open Settings */}
+      {!permissionsGranted && (
+        <TouchableOpacity
+          onPress={openAppSettings}
+          className="bg-[#5f5f5f] py-4 px-6 rounded-lg w-52 mx-auto"
+        >
+          <Text className="text-center font-semibold text-white">Buka Pengaturan</Text>
+        </TouchableOpacity>
+      )}
+
       {/* Location Permission Modal */}
-      <LocationPermissionModal />
-    </>
+      <LocationPermissionModal onPermissionsGranted={handlePermissionsGranted} />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 20,
-    backgroundColor: "#fff",
-  },
-  title: {
-    textAlign: "center",
-    marginBottom: 20,
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#059669", // Trackify branding color
-  },
-  input: {
-    marginBottom: 15,
-    backgroundColor: "#fff",
-  },
-  button: {
-    marginTop: 20,
-    color: "#fff",
-    backgroundColor: "#059669", // Trackify branding color
-  },
-  errorText: {
-    color: "red",
-    textAlign: "center",
-    marginBottom: 15,
-  },
-});
 
 export default LoginScreen;
