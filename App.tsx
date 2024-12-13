3acdcaf2c62339d93be5dbafbffadad5f7a69ac4
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-13 22:28:39 +0700

    Removed ui-kitten, added Nativewind, Redux, SafeAreaProvider, and configured StatusBar

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
+import "./global.css"
 import React from 'react';
-import * as eva from '@eva-design/eva';
+import store from "./src/store/index";
+import { Provider } from 'react-redux';
+import { StatusBar } from "react-native";
 import MainScreen from './src/screens/MainScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  StatusBar.setBarStyle('dark-content');
+  StatusBar.setBackgroundColor('transparent');
+  StatusBar.setTranslucent(true);
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
